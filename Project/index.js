0ad293514a72e26c63c4c8fa08020eeff9d1d756
KUHOO-S <kuhoo1999@rediffmail.com>
2019-11-03 11:30:57 +0530

    trying connection

--- Project/index.js
+++ Project/index.js
@@ -4,7 +4,18 @@ var url = "mongodb://localhost:27017/GravitasDB";
     MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var Hdb = db.db("Hotel Management");
-        data=Hdb.collection("aleya").find();
-        console.log(data);
+        //data=Hdb.collection("aleya").find().toArray();
+        db.collection('aleya').find({}).toArray().then((docs) => {
+
+            console.log(docs);
+
+        }).catch((err) => {
+
+            console.log(err);
+        }).finally(() => {
+
+            client.close();
+        });
+        //console.log(data);
         console.log("hit");
     });
