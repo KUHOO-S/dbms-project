049d06a9e34afef0b5885f270bcf9f0f12fccf50
KUHOO-S <kuhoo1999@rediffmail.com>
2019-11-04 00:12:09 +0530

    node js is added

--- index.js
+++ index.js
@@ -2,28 +2,22 @@ var express= require('express');
 var bodyParser=require('body-parser');
 var app=express();
 app.set('view engine','ejs');
-
 var urlencoded=bodyParser.urlencoded({extended:false});
-app.use('/assets',express.static('assets'));
+
+
+app.use(express.static('./public'));
+
 
  app.get('/home',function(req,res)
- {  console.log(req.query);
-     res.render('contact',{qs: req.query});
+ {
+     res.render('homepage');
  });
 
- app.post('/contact',urlencoded,function(req,res)
- {  console.log(req.body);
-     res.render('contact',{qs: req.query});
+ app.get('/book',function(req,res)
+ {
+     res.render('bookingpage');
  });
-
-app.get('/:nameji',function(req,res){
-//res.sendFile(__dirname+'/hi.html');
-var data={
-    age:70,
-    height:5,
-}
-res.render('index',{nameji:req.params.nameji,data:data});
-});
 
 
 app.listen(3000);
+console.log("it is on");
